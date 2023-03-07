b79d7ca00d77f98672c65a23af2bbebc2d7ee041
Federico <federico.capaldo@hotmail.com>
2023-03-07 03:12:40 +0000

    actually updating details of film

--- app/javascript/controllers/edit_movie_controller.js
+++ app/javascript/controllers/edit_movie_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="edit-movie"
 export default class extends Controller {
-  static targets = ["infos", "form"]
+  static targets = ["infos", "form", "card"]
 
   connect() {
   }
@@ -10,5 +10,17 @@ export default class extends Controller {
   displayForm() {
     this.infosTarget.classList.add("d-none")
     this.formTarget.classList.remove("d-none")
+  }
+
+  update(event) {
+    event.preventDefault();
+    const url = this.formTarget.action;
+    fetch(url, {
+      method: "PATCH",
+      headers: { "Accept": "text/plain" },
+      body: new FormData(this.formTarget)
+    })
+      .then(res => res.text())
+      .then((data) => this.cardTarget.outerHTML = data)
   }
 }
