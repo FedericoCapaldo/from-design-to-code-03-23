0026c9cf54ab9c222563426d25f7648c8de220d6
Federico <federico.capaldo@hotmail.com>
2023-03-07 11:31:03 +0000

    added ajax examples to really understand what's going on when we make a request

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: "movies#index"
   resources :movies, only: [:index, :update]
+
+  # ajax examples: three pages that do the same thing (a simple ajax request) in 3 different ways.
+  get 'xhr-example', to: 'pages#xhr_example'
+  get 'fetch-example', to: 'pages#fetch_example'
+  get 'fetch-example-modern', to: 'pages#fetch_example_modern'
 end
