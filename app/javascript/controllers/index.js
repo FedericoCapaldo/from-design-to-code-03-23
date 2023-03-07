a9eda5227cde644631c3a2b80dedb0c40065793c
Federico <federico.capaldo@hotmail.com>
2023-03-07 03:06:27 +0000

    toggling display of movie update form

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import EditMovieController from "./edit_movie_controller"
+application.register("edit-movie", EditMovieController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
